4ffaee97b284119d21fd304b59681b9259d2ec43
Nhan Dang <nhan@missingcorner.com>
2016-10-25 10:14:15 +0200

    fix example

--- examples/github.js
+++ examples/github.js
@@ -2,7 +2,10 @@ import GithubUser from './resources/GithubUser'
 
 import client from '../index'
 
+client.setHost('https://api.github.com', 443)
+
 client.setResources({ GithubUser }) //Invoke once only
+
 
 client.githubUser.getInfo('akitoeki')
   .then(res => {
